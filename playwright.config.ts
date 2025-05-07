d01a66f1bbb926ea6f4580a6337d6d9fcb054add
atabatyr <50123743+atabatyr@users.noreply.github.com>
2025-05-07 13:03:19 -0500

    Add junit reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests",
   reporter: [
     ["html", { outputFolder: "playwright-report", open: "never" }],
     ["junit", { outputFile: "results.xml" }],
