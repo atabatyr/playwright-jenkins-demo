36a54e28e3444d229a5fa034da61a54b1c6c5ffc
atabatyr <50123743+atabatyr@users.noreply.github.com>
2025-05-07 10:19:21 -0500

    editing config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  use: {
-    headless: true,
-  },
-  reporter: [["html"]],
-  projects: [
-    {
-      name: "chromium",
-      use: { browserName: "chromium" },
-    },
-  ],
+  reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
+  projects: [{ name: "chromium", use: { browserName: "chromium" } }],
 });
