6344b0ebe5793bb0ffb5c4f4656dc1d8e0fccc82
atabatyr <50123743+atabatyr@users.noreply.github.com>
2025-05-08 09:24:53 -0500

    allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   reporter: [
+    ["list"], // Optional: shows progress in CLI
     ["html", { outputFolder: "playwright-report", open: "never" }],
-    ["junit", { outputFile: "test-results/results.xml" }], // ✅ THIS LINE IS CRUCIAL
+    ["junit", { outputFile: "test-results/results.xml" }],
+    ["allure-playwright"],
   ],
   projects: [
     {
