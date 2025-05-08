9d134437e567cd1de93e76b13717fc7927606ff2
atabatyr <50123743+atabatyr@users.noreply.github.com>
2025-05-08 09:06:38 -0500

    config file and package file updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   reporter: [
     ["html", { outputFolder: "playwright-report", open: "never" }],
-    ["junit", { outputFile: "test-results/results.xml" }],
+    ["junit", { outputFile: "test-results/results.xml" }], // ✅ THIS LINE IS CRUCIAL
   ],
   projects: [
     {
