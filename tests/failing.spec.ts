21df77d12735b410e4694ff67850591dc6eabf75
atabatyr <50123743+atabatyr@users.noreply.github.com>
2025-05-08 10:36:27 -0500

    failing test is back

--- tests/failing.spec.ts
+++ tests/failing.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from "@playwright/test";
 test("should fail intentionally", async ({ page }) => {
   await page.goto("https://playwright.dev");
   // This will fail because the title does NOT contain "Nonexistent"
-  //await expect(page).toHaveTitle(/Nonexistent/);
+  await expect(page).toHaveTitle(/Nonexistent/);
 });
